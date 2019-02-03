d57fe1f21d9e68462e96c8eae8f728e2b2261d40
NeighNays <45363904+NeighNays@users.noreply.github.com>
2019-02-03 21:55:54 +0000

    03-Feb

--- app/src/main/java/com/nathan/blockchaintutorial/MainActivity.java
+++ app/src/main/java/com/nathan/blockchaintutorial/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nathan.blockchaintutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button knowledgeActivity = (Button) findViewById(R.id.testButton);
+        knowledgeActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,knowledge_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button blockchainActivity = (Button) findViewById(R.id.blockchainButton);
+        blockchainActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, blockchain_activity.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
+
